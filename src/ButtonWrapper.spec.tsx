31050f18b300be484e02b8e8b5941ab9ddcf418b
Asel Williams <aselwilliams22@gmail.com>
2023-04-29 11:44:14 -0500

    testing state hooks

--- src/ButtonWrapper.spec.tsx
+++ src/ButtonWrapper.spec.tsx
@@ -5,7 +5,7 @@ import {ButtonWrapper} from './ButtonWrapper';
 test('handles onClick', ()=> {
     const onClick = jest.fn();
     render(<ButtonWrapper onClick={onClick} title="Add Item" />);
-    const buttonElement = screen.getByRole('button', 'Add Item');
+    const buttonElement = screen.getByRole('button', /add item/i);
     fireEvent.click(buttonElement);
     expect(onClick).toHaveBeenCalledTimes(1);
 })
